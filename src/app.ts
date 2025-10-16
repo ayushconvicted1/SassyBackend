4948fc25e80a567ba013d6374ec09e53770b0d7b
Ayush Pandey <ayushconvicted@gmail.com>
2025-10-16 17:46:44 +0530

    Changes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import { CorsOptions } from "cors";
 import express from "express";
 const cors = require("cors");
 const morgan = require("morgan");
@@ -15,7 +16,28 @@ const reviewRoutes = require("./routes/review.routes");
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "https://www.sassyshringaar.com",
+  "https://sassyshringaar.com",
+];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.options("*", cors(corsOptions));
+
+// This line handles all the other actual requests
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 
